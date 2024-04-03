104c1b00371e81104f2e11a31b11722515390e94
Vladyslav Salenik <vladyslav.salenik@gmail.com>
2024-04-03 16:43:49 +0300

    1

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -3,9 +3,8 @@ import s from "./SearchBox.module.css";
 function SearchBox({ search, handleSearch }) {
   return (
     <div>
-      <label htmlFor="searchInput">
-        {" "}
-        find contacts by name
+      <label htmlFor="searchInput" className={s.label}>
+        <span>find contacts by name</span>
         <input
           type="text"
           value={search}
